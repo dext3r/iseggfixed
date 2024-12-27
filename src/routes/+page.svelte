4a6e5b2cf1569ea52507b72fadc2b310c612daaa
Bryan Kaminski <dext0rb@Mac.localdomain>
2024-12-26 18:50:11 -0600

    egg always bad

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -5,3 +5,6 @@
 
 <h1>Welcome to is Egg Fixed</h1>
 <p>Egg Bad thats why we need a fascist.</p>
+
+<p>EVERY DAY I AM CHECKING EGG PRICE AND PRICE IS BAD FOREVER??</p>
+<h2>CAN DEV DO SOMETHING???</h2>
