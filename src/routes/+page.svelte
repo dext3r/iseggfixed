1c0b4ef5a394a001a59b89fe9206898fac26bca5
Bryan Kaminski <dext0rb@Mac.localdomain>
2025-02-01 00:34:14 -0600

    added img?

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -10,3 +10,5 @@
 <h2>CAN DEV DO SOMETHING???</h2>
 
 <h1>Nov 2024 we are $3.649 for 12 beautiful egg. (Ask FRED about it)</h1>
+
+<img src="https://hqyrgkrbwejwdpoenaug.supabase.co/storage/v1/object/public/docs//Untitled.jpg" />
